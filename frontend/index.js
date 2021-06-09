6005e7374db67d05385605e42ac891d2778081f6
Romain Cahuet <romain.cahuet@epita.fr>
2021-06-10 00:31:14 +0200

    [FRONTEND][UPDT] index.js: added the necessary callbacks to update the text

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,15 @@
-
 var socket = io("http://127.0.0.1:5000");
 
 socket.on('connect', function() {
+    socket.emit("update counter", false)
     socket.emit('my event', {data: 'I\'m connected!'});
 });
+
+socket.on("update counter", function(count){
+    var myCounter = document.getElementById("counter");
+    myCounter.textContent = String(count);
+});
+
+function clickEvent() {
+    socket.emit("update counter", true);
+}
