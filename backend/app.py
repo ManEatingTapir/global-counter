b469c3f9e361f3a77466fd1cb6b345b1256362ae
Romain Cahuet <romain.cahuet@epita.fr>
2021-06-09 21:31:38 +0200

    [BACKEND][MOD] app.py: enabled Cors, added a listener

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,11 @@ from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@socketio.on("connect")
+def handle_connect(msg):
+    print(f"received connect")
 
 if __name__ == '__main__':
     socketio.run(app)
