b2ac051ecf4f7a18bf38b9d2730ed24ba5d8e26b
Romain Cahuet <romain.cahuet@epita.fr>
2021-06-10 00:31:50 +0200

    [BACKEND][UPDT] app.py: added the handlers for the button press event

--- backend/app.py
+++ backend/app.py
@@ -3,11 +3,24 @@ from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
+app.config["counter"] = 0
 socketio = SocketIO(app, cors_allowed_origins="*")
+
 
 @socketio.on("connect")
 def handle_connect(msg):
     print(f"received connect")
 
+@socketio.on("my event")
+def handle_custom(msg):
+    print("received a custom")
+    print(msg)
+
+@socketio.on("update counter")
+def handle_update(is_change):
+    if is_change:
+        app.config["counter"] += 1
+    socketio.emit("update counter", str(app.config["counter"]), broadcast = True)
+
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, debug = True)
