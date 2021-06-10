1115b9a1b3a9543f02abbf43f8ac11a37bdfab93
David Spivey <davidspivey3.1415@gmail.com>
2021-06-10 12:04:30 -0500

    Remove listener for update, add logic to make counter update handle initial connection

--- backend/app.py
+++ backend/app.py
@@ -9,16 +9,13 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 @socketio.on("connect")
 def handle_connect(msg):
     print(f"received connect")
-    return app.config['clicks']
-
-@socketio.on('button')
-def handle_button(msg):
-    app.config['clicks'] += 1
-    socketio.emit('increment', app.config['clicks'])
 
 @socketio.on('counter update')
-def handle_counter_update():
+def handle_counter_update(is_click):
+    if is_click:
+        app.config['clicks'] += 1
     socketio.emit('update', app.config['clicks'])
+
 if __name__ == '__main__':
     socketio.run(app)
 
