5b364579694a4b23511a0aea730da73d455076c2
David Spivey <davidspivey3.1415@gmail.com>
2021-06-10 11:22:01 -0500

    Add ctracker for button and handlers for counter update

--- backend/app.py
+++ backend/app.py
@@ -3,15 +3,22 @@ from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
+app.config['clicks'] = 0;
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 @socketio.on("connect")
 def handle_connect(msg):
     print(f"received connect")
+    return app.config['clicks']
 
 @socketio.on('button')
-def handle_custom(msg):
-    print(msg)
+def handle_button(msg):
+    app.config['clicks'] += 1
+    socketio.emit('increment', app.config['clicks'])
 
+@socketio.on('counter update')
+def handle_counter_update():
+    socketio.emit('update', app.config['clicks'])
 if __name__ == '__main__':
     socketio.run(app)
+
