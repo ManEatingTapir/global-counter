d46618c694bddff44b63483e2dc378e44758a479
David Spivey <davidspivey3.1415@gmail.com>
2021-06-14 16:29:38 -0500

    Create display component

---
+++ frontend/src/app/counterdisplay/counterdisplay.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counterdisplay',
+  templateUrl: './counterdisplay.component.html',
+  styleUrls: ['./counterdisplay.component.css']
+})
+export class CounterDisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
