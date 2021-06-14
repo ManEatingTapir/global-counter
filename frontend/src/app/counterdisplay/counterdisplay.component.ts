a2533bdd811dcb6de481093831ce128f562f5871
David Spivey <davidspivey3.1415@gmail.com>
2021-06-14 17:02:47 -0500

    Update CounterDisplayComponent with template and counter binding

--- frontend/src/app/counterdisplay/counterdisplay.component.ts
+++ frontend/src/app/counterdisplay/counterdisplay.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { io } from 'socket.io-client';
 @Component({
   selector: 'app-counterdisplay',
   templateUrl: './counterdisplay.component.html',
   styleUrls: ['./counterdisplay.component.css']
 })
 export class CounterDisplayComponent implements OnInit {
-
+  counter = 0;
   constructor() { }
 
   ngOnInit(): void {
